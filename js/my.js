c8ce0a6867de8dbfb28fb1885fd456c09d4fb57a
riskgame15 <tttungcr@gmail.com>
2024-08-09 16:38:45 +0700

    thuc hanh khai bao va sd bien

---
+++ js/my.js
@@ -0,0 +1,33 @@
+//  chu thich
+/* phan biet cac event
+
+-- event tac dong tu con chuot may tinh
+    + click
+    + move
+-- su kien tu ban phim
+    + input
+-- su kien tu tac vao doi tuong window
+ */
+// bài tập tính số tiền nhận được với lãi suất y%/năm
+/* let x= +prompt("nhap so tien muon gui");
+let y= +prompt("nhap so thang gui");
+
+let g = 5/100;
+let moneyOneMonth = g/12*x;
+let totalMoney = x + y * moneyOneMonth;
+
+document.write("Tong so tien nhan lai: " + totalMoney ); */
+
+
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f);
+document.write('<br/>')
+document.write('b = ' + b);
+document.write('<br/>')
+document.write('s = ' + s );
+
