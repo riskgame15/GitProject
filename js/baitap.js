34509322febd364a490b56b4858cfdff87a84275
riskgame15 <tttungcr@gmail.com>
2024-08-12 09:31:42 +0700

    done

---
+++ js/baitap.js
@@ -0,0 +1,20 @@
+/* BAI 1
+function showMessage() {
+    alert('xin chao');
+}
+BAI 2
+function showMessage() {
+    document.getElementById("messageDiv").innerHTML ='xin chao';
+}
+
+Bai 3
+function showMessage() {
+    document.write('Xin chao');
+}
+Bai 4
+
+
+function showMessage() {
+    console.log('Xin chao');
+}
+ */
