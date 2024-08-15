8de7f51874c6ca3374dbf1cd44202b6fbc218086
riskgame15 <tttungcr@gmail.com>
2024-08-15 11:18:34 +0700

    su kien ban phim

--- ctdk1/dk1.js
+++ ctdk1/dk1.js
@@ -95,7 +95,7 @@ let message = (login == 'Employee') ?
             'No login' :
             '';
 alert(message);
- */
+
 let imgObj = null;
 imgObj = document.getElementById('myImage');
 function init() {
@@ -107,4 +107,40 @@ function moveRight() {
     imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
 }
 window.onload = init;
+ */
+function upArrowPressed() {
+    let element = document.getElementById('nobita');
+    element.style.top = parseInt(element.style.top) - 5 + 'px';
+}
+function downArrowPressed() {
+    let element = document.getElementById('nobita');
+    element.style.top = parseInt(element.style.top) + 5 + 'px';
+}
+function leftArrowPressed() {
+    let element = document.getElementById('nobita');
+    element.style.left = parseInt(element.style.left) - 5 + 'px';
+}
+function rightArrowPressed() {
+    let element = document.getElementById('nobita');
+    element.style.left = parseInt(element.style.left) + 5 + 'px';
+}
+function moveSelection(evt){
+    switch(evt.keyCode){
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
 
+    }
+}
+function docReady(){
+    window.addEventListener('ketdown', moveSelection);
+}
