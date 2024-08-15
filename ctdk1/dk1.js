09248ba32d8a0e4574bb09d48a27dcf5a283467c
riskgame15 <tttungcr@gmail.com>
2024-08-15 10:02:01 +0700

    luyen tap

--- ctdk1/dk1.js
+++ ctdk1/dk1.js
@@ -13,10 +13,27 @@ if (year % 4 === 0) {
 }else {
     alert(`is not a leap year`);
 }
- */
+
 let value = prompt('What is the "official" name of JavaScript?','');
 if (value == 'ECMAScript') {
     alert('Right!');
 } else {
     alert("You don't know? ECMAScript!");
+}
+ */
+let useName = prompt("who's there", '')
+if (useName == 'Admin'){
+    let pass = prompt("password", '');
+    if (pass == 'TheMaster'){
+        alert("Welcome");
+    }else if(pass == null){
+        alert("Canceled");
+    }else{
+        alert('Wrong password');
+    }
+
+}else if (useName == null){
+    alert("Canceled");
+}else {
+    alert("I don't know you");
 }
