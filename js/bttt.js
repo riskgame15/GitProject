22814212c5f89b4574817f3e70e247ca29d2a9ef
riskgame15 <tttungcr@gmail.com>
2024-08-16 09:26:13 +0700

    bai tap thuc hanh

--- js/bttt.js
+++ js/bttt.js
@@ -27,7 +27,7 @@ document.write('<br>tong diem ' + total)
 // document.write('chu vi hinh tron ' + chuvi)
 // document.write('dien tich hinh tron ' + dientich)
 
-
+/*
 function changeMoney(){
     let amount = document.getElementById("amount").value;
     let fromCurrency = document.getElementById("from").value;
@@ -44,3 +44,62 @@ function changeMoney(){
     }
     document.getElementById("result").innerHTML = result;
 }
+ */
+// let n = parseInt(prompt('Nhập n số nguyên tố đầu tiên'))
+// if (n === 2) {
+//     document.write('n la so nguyen to')
+// }
+// for (let i = 2; i < n; i++)
+/*
+bai 1
+ */
+
+
+
+Bai 1
+let i = 1
+for (i = 1; i <= 20; i++) {
+    console.log(i)
+}
+bai 2
+
+let i = 2;
+let sum = 0;
+    while (i <= 50) {
+        console.log(i);
+        sum += i;
+        i += 2;
+    }
+alert(sum);
+ Bai 3
+
+let i = 1;
+let sum = 0;
+for (i <= 99; i += 2) {
+    console.log(i);
+    sum += i;
+}
+alert(sum);
+
+Bai 4
+let n = +prompt('so nguyen duong')
+let gt = 1;
+for ( let i = 1; i <= n; i++) {
+    console.log(i);
+    gt *= i;
+}
+alert(n + '! = ' + gt)
+
+bai 5
+let a = +prompt('so 1');
+let b = +prompt('so 2');
+let result;
+for ( let i = 1; i <= a; i++) {
+    if (a % i == 0 && b % i == 0) {
+        console.log(i);
+    }
+}
+
+
+
+
