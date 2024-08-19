e547486e3c555dfcbab8a788f3cc7a17f9c2cb45
riskgame15 <tttungcr@gmail.com>
2024-08-19 09:26:49 +0700

    tao va thao tac vs mang

---
+++ array/array.js
@@ -0,0 +1,46 @@
+/*
+them phan tu vao cuoi mang
+arr.push
+lay ra va tra ve phan tu cuoi mang
+arr.pop()
+lay ra va tra ve phan tu dau mang
+arr.shift()
+them phan tu vao dau mang
+arr.unshift()
+hoat dong xoa phan tu bat dau tu vi tri 0 va tra ve 1 mang chua cac phan tu da xoa
+arr.splice()
+
+tim kiem phan tu trong co gia tri can tim la x
+su dung ham filter de loc cac phan tu trong mang theo 1 dieu kien,tra ve 1 mang moi chua cac phan tu thoa man
+arr.filter(value)
+ */
+
+/*
+let array = [1, 2, 4, 6, 7, 10, 1];
+let x = prompt('nhập giá trị cần tìm');
+let count = 0;
+for (const value of array) {
+    if (value == x){
+    console.log(value);
+    count++
+    }
+}
+alert(count)
+ */
+
+let x = 0;
+let array = Array();
+function addelement(){
+    array[x] = document.getElementById('txtValue').value;
+    alert('Element: ' + array[x] + 'Added at index ' + x);
+    x++;
+    document.getElementById('txtValue').value = '';
+}
+function hienthi(){
+    let e = '<hr/>';
+    for(let i = 0; i < array.length; i++){
+        e += 'Element ' + i + ' = ' + array[i] + '<br/>';
+    }
+    document.getElementById('result').innerHTML = e;
+}
+
